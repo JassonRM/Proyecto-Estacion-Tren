29faaebab0f33a1159b8f33c74e0f74f584afbf9
JassonRM <jassonrm@icloud.com>
2017-06-07 19:36:36 -0600

    Optimizacion

--- Estacion Tren.py
+++ Estacion Tren.py
@@ -31,22 +31,57 @@ class Tren:
         self.id = id
         self.ruta = ruta
         self.hora = hora
+        self.demanda = random.randrange(5, 75)
         if ruta.find("TEC") == 0:
             self.enEstacion = True
             self.maquina = None
             self.head = None
             self.tail = None
             self.carga = 0
-            self.demanda = None
+            self.capacidad = 0
         else:
             self.enEstacion = False
-            self.demanda = random.randrange(5, 75)
-            #Codigo para optimizar la maquina y los vagones aplica aqui tambien
-            self.maquina = Maquina()
+            self.optimizar(self.demanda)
+
+    def optimizar(self, demanda):
+        if self.enEstacion:
+            listaVagones = vagonesLibres
+            listaMaquinas = maquinasLibres
+        else:
+            listaVagones = vagonesFuera
+            listaMaquinas = maquinasFuera
+
+        vagones = []
+        while demanda > 0:
+            mejorVagon = None
+            for vagon in listaVagones:
+                if vagon.capacidad >= demanda and (mejorVagon == None or vagon.capacidad < mejorVagon.capacidad):
+                    mejorVagon = vagon
+                    if vagon.capacidad == demanda:
+                        break
+            if mejorVagon == None:
+                for vagon in listaVagones:
+                    if mejorVagon == None or demanda - vagon.capacidad < demanda - mejorVagon.capacidad:
+                        mejorVagon = vagon
+            vagones.append(mejorVagon)
+            demanda -= mejorVagon.capacidad
+        mejorMaquina = None
+        for maquina in listaMaquinas:
+            if maquina.capacidad >= len(vagones) and (mejorMaquina == None or maquina.capacidad < mejorMaquina.capacidad):
+                mejorMaquina = maquina
+                if mejorMaquina.capacidad == len(vagones):
+                    break
+        self.asignarMaquina(mejorMaquina.id)
+        for vagon in vagones:
+            self.engancharInicio(vagon.id)
 
 
-    def asignarMaquina(self, id):
-        for maquina in maquinasLibres:
+    def asignarMaquina(self, id): #Acepta trenes en la estacion y afuera
+        if self.enEstacion:
+            listaMaquinas = maquinasLibres
+        else:
+            listaMaquinas = maquinasFuera
+        for maquina in listaMaquinas:
             if maquina.id == id:
                 if self.maquina != None:
                     maquinasLibres.append(self.maquina)
@@ -55,9 +90,14 @@ class Tren:
             elif maquina == maquinasLibres[-1]:
                 print("Máquina no encontrada")
 
-    def engancharInicio(self, id):
+    def engancharInicio(self, id): #Acepta trenes en la estacion y afuera
+        if self.enEstacion:
+            listaVagones = vagonesLibres
+        else:
+            listaVagones = vagonesFuera
+
         if self.carga != self.maquina.capacidad and self.maquina != None:
-            for vagon in vagonesLibres:
+            for vagon in listaVagones:
                 if vagon.id == id:
                     temp = self.head
                     self.head = vagon
@@ -67,6 +107,7 @@ class Tren:
                     else:
                         self.tail = self.head
                     self.carga += 1
+                    self.capacidad += vagon.capacidad
                     vagonesLibres.remove(vagon)
                     break
                 elif vagon == vagonesLibres[-1]:
@@ -76,7 +117,7 @@ class Tren:
         else:
             print("Capacidad de la máquina alcanzada")
 
-    def engancharMedio(self, id, pos):
+    def engancharMedio(self, id, pos): #Solo trenes en la estacion
         if self.carga != self.maquina.capacidad and self.maquina != None:
             for vagon in vagonesLibres:
                 if vagon.id == id:
@@ -95,6 +136,7 @@ class Tren:
                         vagon.prev = temp.prev
                         vagon.next = temp
                         self.carga += 1
+                        self.capacidad += vagon.capacidad
                         vagonesLibres.remove(vagon)
                     else:
                         print("Índice fuera de rango")
@@ -107,12 +149,13 @@ class Tren:
             print("Capacidad de la máquina alcanzada")
 
     def engancharFinal(self, id):
-
+        print("Nuevo final")
     def quitarVagon(self, pos):
-
+        print("Quitar vagonl")
     def salir(self):
-
+        print("salir")
     def llegar(self):
+        print("Llegar")
 
     def mostrar(self):
         print("ID : ", self.id)
@@ -147,11 +190,13 @@ class Vagon:
 trains = []
 newTrainID = 0
 
+
 with open("estacion.txt") as config:
     vagonesLibres = eval(config.readline())
     maquinasLibres = eval(config.readline())
+    vagonesFuera = eval(config.readline())
+    maquinasFuera = eval(config.readline())
     for line in config:
-        print(line)
         if line.find("Ruta") != -1:
             ruta = line.replace("Ruta ", "")
         else:
@@ -167,6 +212,5 @@ pygame.init()
 windowWidth = pygame.display.Info().current_w
 windowHeight = pygame.display.Info().current_h
 
-trains[0].asignarMaquina(10)
-trains[0].engancharInicio(100)
+
 

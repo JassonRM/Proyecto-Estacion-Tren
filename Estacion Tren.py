b143616d1780284dddb3fa83201928eae9878fd6
m-herrera <m.herrera0799@gmail.com>
2017-06-11 12:47:50 -0600

    cambio tkinter

--- Estacion Tren.py
+++ Estacion Tren.py
@@ -5,30 +5,24 @@
 
 import sys
 import os
+from tkinter import *
 import pygame
 import random
 from threading import Thread
 import time
 import datetime
-
-#Definir colores
-blue = (0,0,255)
-black = (0,0,0)
-white = (255,255,255)
-
-pygame.init()
-
-windowWidth = pygame.display.Info().current_w
-windowHeight = pygame.display.Info().current_h
-fontSize = int(windowWidth/32) # tamaño de letra
+from PIL import Image, ImageTk
 
 #Funcion: cargarImagen
 #Entrada: nombre
 #Salida: imagen
 #Restricciones: el archivo debe estar contenido en la carpeta Imagenes
-def cargarImagen(nombre):
+
+def cargarImagen(nombre,tamaño):
     ruta = os.path.join("Imagenes", nombre)
-    imagen = pygame.image.load(ruta)
+    imagen = Image.open(ruta)
+    imagen.thumbnail((tamaño,tamaño*9//16))
+    imagen = ImageTk.PhotoImage(image = imagen)
     return imagen
 
 #Funcion: cargarSonido
@@ -46,46 +40,8 @@ def cargarSonido(nombre):
 #Restricciones: imagen de tipo pygame, ancho y altura son enteros
 def scale_img(image,width,height):    
     image = pygame.transform.smoothscale(image,(width,height))
-    return image
+    return PhotoImage(image)
 
-class Text:
-    def __init__(self,font = 'microsoftyaheimicrosoftyaheiui',bold = False,italic = False,size = fontSize,underline = False, texto = ""):
-        self.font = font
-        self.bold = bold
-        self.italic = italic
-        self.underline = underline
-        self.size = size
-        self.space = (0,0)
-        self.color = white
-        self.texto = texto
-
-    #Metodo: render
-    #Entrada: texto a mostrar, posición de la superficie donde se desea centrar,
-    #posición de la superficie donde se desea colocar
-    #Salida: texto listo para mostrar y tupla con posicion del texto
-    #Restricciones: posiciones deben ser tuplas
-    def render(self,texto = None,bSize=(0,0),bDimensions=(0,0)):
-        if texto == None:
-            texto = self.texto
-        font = pygame.font.SysFont(self.font,self.size)
-        self.space = font.size(texto)
-        font.set_bold(self.bold)
-        font.set_italic(self.italic)
-        font.set_underline(self.underline)
-        text = font.render(texto,True,self.color)
-        placex = bDimensions[0] + bSize[0]//2 - self.space[0]//2
-        placey = bDimensions[1] + bSize[1]//2 - self.space[1]//2
-        return text,(placex,placey)
-
-    #Metodo: set_color
-    #Entrada: color
-    #Salida: Cambia el color de un texto
-    #Restricciones: color debe ser RGB
-    def set_color(self,color):
-        self.color = color
-
-    def mostrar(self):
-        print(self.texto)
 
 class Tren:
     def __init__(self, id, ruta, hora):
@@ -392,207 +348,69 @@ def mostrar(lista):
 
 """__________________________________________________________________________"""
 
-
-#inicializar Clock
-clock = pygame.time.Clock()
+estacion = True
+in_rutas = False
 
 #Crear ventana 
-ventana = pygame.display.set_mode((windowWidth,windowHeight), pygame.FULLSCREEN)
-pygame.display.set_caption("Estación TEC")
+ventana = Tk()
+windowWidth = ventana.winfo_screenwidth()
+windowHeight = ventana.winfo_screenheight()
+ventana.overrideredirect(True)
+ventana.geometry("%dx%d+0+0" %(windowWidth,windowHeight))
+ventana.title("Estación TEC")
+c_ventana = Canvas(ventana)
+c_ventana.pack(fill=BOTH, expand=True)
+boton = cargarImagen("boton.png",windowWidth//10)
+font = "Courier New"
+bfSize = 16
 
-bSize = (windowWidth*0.225,windowHeight*0.225) # tamaño botones
-
-
-#Funcion: buttonPressed
-#Entrada: posicion del cursor y cordenadas rectangulares para revisar
-#Salidas: Verifica si el cursor se encuentra entre las cordenadas
-#restricciones: cordenadas son rectangulares
-def buttonPressed(rect,mouse):
-    left = rect[0]
-    top = rect[1]
-    width = rect[2]
-    height = rect[3]
-    if mouse[0] > left and mouse[0] < left + width and mouse[1] > top and mouse[1] < top + height:
-        return True
-    else:
-        return False
-
-#ciclo de Menu
-def Menu_loop():
-    global enEjecucion
-    in_menu = True
-    
-    #cargar imagen de fondo
-    fondoMenu = scale_img(cargarImagen("fondo.jpg"),windowWidth,windowHeight)
-
-    #Definir posición de botones 
-    boton_top = int(windowHeight*5/24)
-    boton_left = int(windowWidth*15/256)
-    boton_bottom = int(windowHeight*16/29)
-    boton_right = int(windowWidth*131/183)
-
-    # posición del texto time
-    Time = (int(windowWidth*0.5),int(windowHeight*0.1))
-
-    #crear Texto de Hora
-    text_time = Text()
-    
-    #textos = [(posicionx,posiciony,texto,comando)]
-    textos = [(boton_left,boton_top,Text(texto = "Lista Rutas"),Lista_Rutas_loop),(boton_right,boton_top,Text(texto = "Estación"),Demanda_loop),
-              (boton_right,boton_bottom,Text(texto = "Llegadas/"),Llegadas_loop),(boton_right,boton_bottom,Text(texto = "Salidas"),Llegadas_loop),
-              (boton_left,boton_bottom,Text(texto = "Administrar"),Administrar_loop),(boton_left,boton_bottom,Text(texto = "Vagones"),Administrar_loop)]
-
-
-    #boton = (cordenada rectangular,texto,comando)
-    botones = []
-    for texto in textos:
-        botones.append((pygame.Rect(texto[0],texto[1],bSize[0],bSize[1]),texto[2],texto[3]))
-
-    #ciclo principal de ventana
-    while in_menu:
-            #cerrar ventana
-            for event in pygame.event.get():
-                if event.type ==pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        enEjecucion = False
-                        pygame.quit()
-                        sys.exit()    
-    
-            #revisa estado de todos los botones 
-            for boton in botones:
-                if buttonPressed(boton[0],pygame.mouse.get_pos()):
-                #ejecuta la funcion respectiva para cada boton
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        boton[2]()
-                        in_menu = False
-                        break
-                    #cambia color de texto
-                    boton[1].set_color(white)
-                else:
-                    boton[1].set_color(black)
-            
-            #actualiza la hora
-            clock = "Hora: " + str(time.asctime())[10:-4] + "/ Fecha:" + str(time.asctime())[3:10] + str(time.asctime())[-5:]
-            clock = text_time.render(texto = clock,bDimensions = Time)
-
-            #Muestra el fondo y la hora
-            ventana.blit(fondoMenu,(0,0))
-            ventana.blit(clock[0],clock[1])
-
-            #actualiza y muestra textos
-            for boton in botones:
-                if boton[1].texto == "Administrar" or boton[1].texto == "Llegadas/":
-                    mensaje = boton[1].render(bSize = bSize,bDimensions = (boton[0][0],boton[0][1]-fontSize//2))
-                elif boton[1].texto == "Vagones" or  boton[1].texto == "Salidas":
-                    mensaje = boton[1].render(bSize = bSize,bDimensions = (boton[0][0],boton[0][1]+fontSize//2))
-                else:
-                    mensaje = boton[1].render(bSize = bSize,bDimensions = (boton[0][0],boton[0][1]))
-                ventana.blit(mensaje[0],mensaje[1])
-
-            #Actualiza ventana
-            pygame.display.update()
-
-    
-#ciclo de Listas de Rutas
-def Lista_Rutas_loop():
-    global enEjecucion
-    in_rutas = True
-    fondo = scale_img(cargarImagen("metallic2.jpg"), windowWidth, windowHeight)
-    textos = [Text(size=windowWidth // 20, texto="Rutas de hoy" )]
-    ultimaRuta = trains[0].ruta.replace("\n", "")
-    textos.append(Text(texto=ultimaRuta))
-    horas = ""
-    for tren in trains:
-        if tren.ruta.replace("\n", "") != ultimaRuta:
-            ultimaRuta = tren.ruta.replace("\n", "")
-            textos.append(Text(texto=horas))
-            textos.append(Text(texto=ultimaRuta))
-            horas = ""
-        if tren.hora[1] < 10:
-            minutos = "0" + str(tren.hora[1])
-        horas += str(tren.hora[0]) + ":" + minutos + " "
-    textos.append(Text(texto=horas.replace("\n","", )))
-
+def rutas_estacion():
+    global estacion
+    global in_rutas
+    if in_rutas:
+        in_rutas = False
+        estacion = True
+    elif estacion:
+        estacion = False
+        in_rutas = True
+        
+def rutas_loop():
+    rutas = Toplevel()
+    rutas.geometry("%dx%d+0+0" %(windowWidth,windowHeight))
+    c_rutas = Canvas(rutas)
+    c_rutas.pack(fill=BOTH, expand=True)
+    rutas.overrideredirect(True)
+    fondo = cargarImagen("metallic2.png",windowWidth)
+    boton_estacion = Button(c_rutas,image = boton,text = "ESTACIÓN",font = (font,bfSize),compound = "center", activeforeground= "white",command = rutas_estacion)
     while in_rutas:
-        posicionTextos = windowHeight // 10
-        #cerrar ventana
-        iconoAtras = "atras.png"
-        botonAtras = pygame.Rect(windowWidth // 10, windowHeight // 10, windowWidth // 10, windowHeight // 5.62)
-        if buttonPressed(botonAtras, pygame.mouse.get_pos()):
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                Menu_loop()
-            iconoAtras = "atras1.png"
-        else:
-            iconoAtras = "atras.png"
+        c_rutas.create_image(0,0,image = fondo,anchor = NW)
+        c_rutas.create_text(windowWidth//2,windowHeight//2,text = "Rutas de hoy", font = (font, 30),anchor = CENTER)
+        boton_estacion.place(relx = 0.005,rely = 0.01)
+        rutas.update()
+    if estacion:
+        rutas.destroy()
+        estacion_loop()
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    enEjecucion = False
-                    pygame.quit()
-                    sys.exit()
-        ventana.blit(fondo,(0,0))
-        ventana.blit(scale_img(cargarImagen(iconoAtras), windowWidth // 10, int(windowHeight // 5.62)), (windowWidth//10, windowHeight // 10))
-        for texto in textos:
-            rotulo = texto.render(bDimensions=(windowWidth // 2, posicionTextos))
-            ventana.blit(rotulo[0], rotulo[1])
-            posicionTextos += 80
-        pygame.display.update()
-
-#ciclo de Demandas
-def Demanda_loop():
-    global enEjecucion
-    in_demanda = True
-    fondo = scale_img(cargarImagen("metallic2.jpg"), windowWidth, windowHeight)
-    while in_demanda:
-
-        #cerrar ventana
-        for event in pygame.event.get():
-            if event.type ==pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    enEjecucion = False
-                    pygame.quit()
-                    sys.exit()
-                    
+def estacion_loop():
+    ventana.deiconify()
+    
+    fondo = cargarImagen("estacion.png",windowWidth)
+    boton_rutas = Button(ventana,image = boton,text = "RUTAS",font = (font,bfSize),compound = "center", activeforeground= "white",command = rutas_estacion)
+    while estacion:
+        hora = "Hora: " + str(time.asctime())[10:-4] + "/ Fecha:" + str(time.asctime())[3:10] + str(time.asctime())[-5:]
+        c_ventana.create_image(0,0,image = fondo,anchor = NW)
+        c_ventana.create_text(windowWidth//2,windowHeight//10,text = hora, font = (font, 30),anchor = CENTER)
+        boton_rutas.place(relx = 0.005,rely = 0.01)
+        ventana.update()
         
-        ventana.blit(fondo,(0,0))
-        pygame.display.update()
+    if in_rutas:
+        ventana.withdraw()
+        rutas_loop()
+    
+    
 
-#ciclo de Llegadas
-def Llegadas_loop():
-    global enEjecucion
-    in_llegadas = True
-    fondo = scale_img(cargarImagen("metallic2.jpg"), windowWidth, windowHeight)
-    while in_llegadas:
-
-        #cerrar ventana
-        for event in pygame.event.get():
-            if event.type ==pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    enEjecucion = False
-                    pygame.quit()
-                    sys.exit()    
-        
-        ventana.blit(fondo,(0,0))
-        pygame.display.update()
-
-def Administrar_loop():
-    global enEjecucion
-    in_administrar = True
-    fondo = scale_img(cargarImagen("metallic2.jpg"), windowWidth, windowHeight)
-    while in_administrar:
-
-        #cerrar ventana
-        for event in pygame.event.get():
-            if event.type ==pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    enEjecucion = False
-                    pygame.quit()
-                    sys.exit()    
-        
-        ventana.blit(fondo,(0,0))
-        pygame.display.update()
-
+estacion_loop()
+"""
 def reloj():
     while enEjecucion:
         hora = datetime.datetime.now().hour, datetime.datetime.now().minute, datetime.datetime.now().second
@@ -605,5 +423,4 @@ def reloj():
 
 reloj = Thread(target=reloj, args=())
 reloj.start()
-Menu_loop()
-
+"""

ee61ab6457bf13f0a3183ff260a8d50db86379fb
m-herrera <m.herrera0799@gmail.com>
2017-06-17 22:54:06 -0600

    animacion y menu arreglado

--- Estacion Tren.py
+++ Estacion Tren.py
@@ -13,17 +13,22 @@ import time
 import datetime
 from PIL import Image, ImageTk
 
+pygame.mixer.init()
+
 #Funcion: cargarImagen
 #Entrada: nombre
 #Salida: imagen
 #Restricciones: el archivo debe estar contenido en la carpeta Imagenes
-def cargarImagen(nombre,tamaño):
+def cargarImagen(nombre,tamaño,right = False):
     ruta = os.path.join("Imagenes", nombre)
     imagen = Image.open(ruta)
     imagen.thumbnail((tamaño * windowWidth,tamaño * windowHeight))
+    if right:
+        imagen = imagen.transpose(Image.FLIP_LEFT_RIGHT)
     imagen = ImageTk.PhotoImage(image = imagen)
     return imagen
 
+    
 #Funcion: cargarSonido
 #Entrada: nombre
 #Salida: sonido
@@ -550,24 +555,59 @@ def actualizar_hora():
 #Entradas: cantidad de vagones
 #salidas: lleva a cabbo la animacion de llegada de los trenes
 #Restricciones: cantidad entera
-def animacion_llegada(cantidad):
+def animacion_tren(cantidad,enEstacion):
     if cantidad == 0:
         return None
-    c_ventana.maquina = cargarImagen("maquina.png",0.5) 
-    c_ventana.vagon = cargarImagen("vagon.png",0.5)
-    pos = 680
-    v = [[c_ventana.create_image(windowWidth,640,image = c_ventana.maquina, tags = "p",anchor = W),windowWidth]]
-            
-    for ele in range(cantidad):
-        v += [[c_ventana.create_image(windowWidth+pos,640,image = c_ventana.vagon, tags = "p",anchor = W),windowWidth + pos]]
-        pos += 680
 
-    while v[-1][1] + 680> 0: 
-        c_ventana.move("p",-5,0)
-        v[-1][1] -= 5
+    tren = cargarSonido("tren.wav")
+    tren.play()    
+
+    if enEstacion:
+        c_ventana.maquina = cargarImagen("maquina.png",0.5,True) 
+        c_ventana.vagon = cargarImagen("vagon.png",0.5,True)
+        velocidad = 5
+        pos = -windowWidth*0.5
+        altura = windowHeight*0.84
+        v = [[c_ventana.create_image(0,altura,image = c_ventana.maquina, tags = "p",anchor = E),0]]
+        condicion = windowWidth
+    else:
+        c_ventana.maquina = cargarImagen("maquina.png",0.5) 
+        c_ventana.vagon = cargarImagen("vagon.png",0.5)
+        velocidad = -5
+        pos = windowWidth*1.5
+        altura = windowHeight*0.56
+        v = [[c_ventana.create_image(windowWidth,altura,image = c_ventana.maquina, tags = "p",anchor = W),windowWidth]]
+        condicion2 = 0
+        
+    for ele in range(cantidad):
+        if enEstacion:
+            v += [[c_ventana.create_image(pos,altura,image = c_ventana.vagon, tags = "p",anchor = E),pos]]
+            pos -= windowWidth*0.5
+            condicion2 = pos
+            aumento = 1
+        else: 
+            v += [[c_ventana.create_image(pos,altura,image = c_ventana.vagon, tags = "p",anchor = W),pos]]
+            pos += windowWidth*0.5
+            condicion = pos
+            aumento = -1
+    
+    tren.fadeout(2000)
+    while condicion > condicion2:
+        c_ventana.move("p",velocidad,0)
+        condicion -= abs(velocidad)
         time.sleep(0.001)
+        velocidad -= aumento*(1/100)
+        if int(velocidad)  == 0:
+            time.sleep(1)
+            tren.play()
+            tren.fadeout((cantidad+1)*1800)
+            aumento = -aumento
+        elif abs(velocidad) > 5:
+            aumento = 0
+    
     for ele in v:
         c_ventana.delete(ele[0])
+        
     return None
 
 #Declara el tren en uso
@@ -588,7 +628,7 @@ def refresh ():
         tren_menu.set(objeto)
 
     for train in trains:
-        if train.get_hora()[0] == datetime.datetime.now().hour:
+        if train.get_hora()[0] == datetime.datetime.now().hour and train.enEstacion:
             menu["menu"].add_command(label=train, command=lambda tren = train: seleccion(tren))
 
 """__________________________________________________________________________"""
@@ -606,9 +646,15 @@ def timer():
                 trains.remove(tren)
 
             elif tren.enEstacion == False and hora[:2] == tren.hora and hora[2] == 0: #Llega los trenes
-                animacion_llegada(3)
+                animacion_tren(5,False)
+                
         time.sleep(1)
 """__________________________________________________________________________"""
+
+def salir_tren(cant):
+    animacion_salir = Thread(target= animacion_tren,args = (cant,True))
+    animacion_salir.start()
+
 
 #Funcion: cerrar
 #Entrada: ninguna
